9efcce6caac6ce0c30b9253f847ab9098d634f40
Den_k0 <denys.kotovskiy@gmail.com>
2025-02-24 22:18:06 +0200

    implement model for videos

---
+++ models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Integer, String, Text
+from sqlalchemy.orm import Mapped, mapped_column
+
+from database import Base
+
+
+class Video(Base):
+    __tablename__ = "videos"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    youtube_url: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    transcript: Mapped[str] = mapped_column(Text, nullable=False)
+    summary: Mapped[str] = mapped_column(Text, nullable=False)
